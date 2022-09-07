b8e31678065fa632f60e7df38d5bee31f1cb496a
Hatem Mahmoud <hatemhindawi1999@gmail.com>
2022-09-07 18:18:58 +0200

    filter method

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -1,13 +1,31 @@
 import { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 
+
 class BookList extends Component{
+
+    state = {
+        query: "a"
+    }
+   
+    // filterBooks = () => {
+        
+    //  filteredArray = this.props.books.filter(book => { return book.title.toLowerCase().includes(this.state.query.toLowerCase()) })
+    //   console.log(filteredArray)
+    // }
+
+
+
+
     render(){
+
+
         return(
             <Container>
+                {/* <Button onClick={() => filterBooks()} > something</Button> */}
                 <Row>
-                {this.props.books.map((book)=>{
+                {this.props.books.filter(book => { return book.title.toLowerCase().includes(this.state.query.toLowerCase()) }).map((book)=>{
                         return(
                     <Col key={book.asin}>
                   
